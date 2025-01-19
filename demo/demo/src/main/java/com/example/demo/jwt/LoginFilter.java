5fcce9ec388bf1df66d6159f85551145adfa33b8
kangyoon <kang01105@naver.com>
2025-01-19 18:33:50 +0900

    Feat: add JWTUtil Constructor

--- demo/demo/src/main/java/com/example/demo/jwt/LoginFilter.java
+++ demo/demo/src/main/java/com/example/demo/jwt/LoginFilter.java
@@ -11,12 +11,15 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import java.io.IOException;
 
+//login 필터
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
 
-    public LoginFilter(AuthenticationManager authenticationManager) {
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
+        this.jwtUtil=jwtUtil;
     }
 
     @Override
