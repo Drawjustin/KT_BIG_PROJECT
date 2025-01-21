2c9aa9011c8ea65a6fa4dd4b365355de41aefd03
jisung <swwegjisung@gmail.com>
2025-01-21 20:27:25 +0900

    Comment : login slice 주석 추가

--- goverment_front/src/slices/loginSlice.ts
+++ goverment_front/src/slices/loginSlice.ts
@@ -1,28 +1,29 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-//타입 
+//타입 정의
 interface LoginState {
   email: string;
   password: string;
   isLoggedIn: boolean;
 }
 
-//초기화
+//상태 초기값
 const initialState: LoginState = {
   email: '',
   password: '',
   isLoggedIn: false,
 };
 
-
 const loginSlice = createSlice({
   name: 'loginSlice',
   initialState,
   reducers: {
-    setLoginState(state, action: PayloadAction<{ email: string; password: string }>) {
+    // 로그인 상태 업데이트
+    setLoginState(state, action: PayloadAction<{ email: string; password: string }>) { 
       state.email = action.payload.email;
       state.password = action.payload.password;
       state.isLoggedIn = true;
     },
+    //(로그아웃) 로그인 상태를 초기화
     logout(state) {
       state.email = '';
       state.password = '';
