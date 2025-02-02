727e92b5b268a7de6a874a1234cbc0ca3db15556
kangyoon <kang01105@naver.com>
2025-02-02 16:38:09 +0900

    Feat: add validation annotation in JoinDTO, CONTENT:JoinDTO에 유효성 검사 추가

--- demo/demo/src/main/java/com/example/demo/dto/JoinDTO.java
+++ demo/demo/src/main/java/com/example/demo/dto/JoinDTO.java
@@ -2,6 +2,7 @@ package com.example.demo.dto;
 
 import com.example.demo.entity.UserEntity;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -13,8 +14,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class JoinDTO {
+    @Email(message = "올바른 이메일 형식이 아닙니다")
+    @NotBlank(message = "이메일은 필수 입력값입니다")
     private String userEmail;
     private String userId;
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자리여야 합니다")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다")
     private String userPassword;
     private String userName;
     private String userNumber;
