4c91037ae2a992ed0b4a1c9b02c93a320a7ab3a6
kangyoon <kang01105@naver.com>
2025-02-03 14:35:34 +0900

    Feat: add join registor validation, CONTENT: 한글 이름 3글자를 2-5글자로 변경

--- demo/demo/src/main/java/com/example/demo/dto/request/JoinRequest.java
+++ demo/demo/src/main/java/com/example/demo/dto/request/JoinRequest.java
@@ -22,7 +22,7 @@ public record JoinRequest(
         String userPassword,
 
         @NotBlank(message = "이름은 필수 입력값입니다")
-        @Pattern(regexp = "^[가-힣]{3}$", message = "이름은 한글 3글자여야 합니다")
+        @Pattern(regexp = "^[가-힣]{2,5}$", message = "이름은 한글 2~5글자여야 합니다")
         String userName,
 
         @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "올바른 전화번호 형식이 아닙니다")
