170019e82f74c249869043929cd65a72e67329ed
DrawJustin <guswhd903@naver.com>
2025-02-07 23:55:13 +0900

    Fix : application.properties, CONTNET : 환경변수 수정

--- demo/demo/src/main/java/com/example/demo/config/RedisConfig.java
+++ demo/demo/src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,6 +9,11 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
 
     @Bean
     public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -17,9 +23,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory();
-        factory.setHostName("localhost");
-        factory.setPort(6379);
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost,redisPort);
         return factory;
     }
 }
